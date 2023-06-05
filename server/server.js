fbd24ede4f52dd091fc92c6920e6b772b240b335
hfossuo <hfossuo@gmail.com>
2023-06-05 01:21:46 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const restaurantRoutes = require('./routes/restaurantRoutes');
+const userRoutes = require('./routes/userRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/restaurants', restaurantRoutes);
+app.use('/users', userRoutes);
+app.use('/payments', paymentRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
